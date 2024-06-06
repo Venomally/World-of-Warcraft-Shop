6c7cb690fbe17b42f650630e152c7b0a97ee7bab
Amer Cengic <amercengic90@gmail.com>
2024-06-06 23:39:41 +0200

    Added account,characters and mountinbags pages for offers. Added celtic knot design betwine pages. Added when tou select part of a site in nav bar it will position it self on that part of a page.

---
+++ offers.js
@@ -0,0 +1,10 @@
+document.getElementById("Accounts").addEventListener("click", function() {
+    document.location.href = "./Pages/Accounts.html";
+});
+
+document.getElementById("MountsInBags").addEventListener("click", function() {
+    window.location.href = "./Pages/MountsInBags.html";
+});
+document.getElementById("characters").addEventListener("click", function() {
+    window.location.href = "./Pages/characters.html";
+});
